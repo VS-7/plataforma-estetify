418f8e4b499b9f585e81f240086012f6b00cff3b
VS-7 <vitorsergio.ts@gmail.com>
2024-11-13 18:48:26 -0300

    Implementacao ProcedimentoController (Sprint 3)

---
+++ Estetify/src/main/java/com/ifcolab/estetify/controller/ProcedimentoController.java
@@ -0,0 +1,110 @@
+package com.ifcolab.estetify.controller;
+
+import com.ifcolab.estetify.controller.tablemodel.TMViewProcedimento;
+import com.ifcolab.estetify.model.Procedimento;
+import com.ifcolab.estetify.model.Paciente;
+import com.ifcolab.estetify.model.Enfermeira;
+import com.ifcolab.estetify.model.Medico;
+import com.ifcolab.estetify.model.dao.ProcedimentoDAO;
+import com.ifcolab.estetify.model.exceptions.ProcedimentoException;
+import com.ifcolab.estetify.model.valid.ValidateProcedimento;
+import java.time.LocalDateTime;
+import javax.swing.JTable;
+
+public class ProcedimentoController {
+    
+    private ProcedimentoDAO repositorio;
+    
+    public ProcedimentoController() {
+        repositorio = new ProcedimentoDAO();
+    }
+    
+    public void cadastrar(
+            String dataHora,
+            String descricao,
+            String duracao,
+            String valor,
+            String requisitos,
+            String contraindicacoes,
+            Paciente paciente,
+            Enfermeira enfermeira,
+            Medico medico
+    ) {
+        ValidateProcedimento valid = new ValidateProcedimento();
+        Procedimento procedimento = valid.validaCamposEntrada(
+                dataHora,
+                descricao,
+                duracao,
+                valor,
+                requisitos,
+                contraindicacoes,
+                paciente,
+                enfermeira,
+                medico
+        );
+        
+        repositorio.save(procedimento);
+    }
+    
+    public void atualizar(
+            int id,
+            String dataHora,
+            String descricao,
+            String duracao,
+            String valor,
+            String requisitos,
+            String contraindicacoes,
+            Paciente paciente,
+            Enfermeira enfermeira,
+            Medico medico
+    ) {
+        ValidateProcedimento valid = new ValidateProcedimento();
+        Procedimento procedimento = valid.validaCamposEntrada(
+                dataHora,
+                descricao,
+                duracao,
+                valor,
+                requisitos,
+                contraindicacoes,
+                paciente,
+                enfermeira,
+                medico
+        );
+        
+        procedimento.setId(id);
+        repositorio.update(procedimento);
+    }
+    
+    public void excluir(Procedimento procedimento) {
+        if (procedimento != null) {
+            repositorio.delete(procedimento.getId());
+        } else {
+            throw new ProcedimentoException("Erro - Procedimento inexistente.");
+        }
+    }
+    
+    public void atualizarTabela(JTable grd) {
+        TMViewProcedimento tmProcedimento = new TMViewProcedimento(repositorio.findAll());
+        grd.setModel(tmProcedimento);
+    }
+    
+    public void filtrarTabela(JTable grd, String descricao) {
+        TMViewProcedimento tmProcedimento = new TMViewProcedimento(repositorio.filterByDescricao(descricao));
+        grd.setModel(tmProcedimento);
+    }
+    
+    public void filtrarTabelaPorPaciente(JTable grd, int pacienteId) {
+        TMViewProcedimento tmProcedimento = new TMViewProcedimento(repositorio.findByPaciente(pacienteId));
+        grd.setModel(tmProcedimento);
+    }
+    
+    public void filtrarTabelaPorEquipe(JTable grd, int medicoId, int enfermeiraId) {
+        TMViewProcedimento tmProcedimento = new TMViewProcedimento(repositorio.findByEquipe(medicoId, enfermeiraId));
+        grd.setModel(tmProcedimento);
+    }
+    
+    public void filtrarTabelaPorPeriodo(JTable grd, LocalDateTime inicio, LocalDateTime fim) {
+        TMViewProcedimento tmProcedimento = new TMViewProcedimento(repositorio.findByPeriodo(inicio, fim));
+        grd.setModel(tmProcedimento);
+    }
+}
