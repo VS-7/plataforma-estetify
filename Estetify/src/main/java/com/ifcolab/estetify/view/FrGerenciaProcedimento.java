6d05c1b71fcf149462de5c2f561f59d88cdae8a8
VS-7 <vitorsergio.ts@gmail.com>
2024-11-18 17:25:30 -0300

    Atualizando disposicao de componentes View FrGerenciaProcedimento (Sprint 4)

--- Estetify/src/main/java/com/ifcolab/estetify/view/FrGerenciaProcedimento.java
+++ Estetify/src/main/java/com/ifcolab/estetify/view/FrGerenciaProcedimento.java
@@ -123,13 +123,10 @@ public class FrGerenciaProcedimento extends javax.swing.JDialog {
         lblContraIndicacoes = new javax.swing.JLabel();
         tmProcedimentos = new javax.swing.JScrollPane();
         grdProcedimentos = new com.ifcolab.estetify.components.CustomTable();
-        lblLogo = new javax.swing.JLabel();
         lblBackgroundTabela = new javax.swing.JLabel();
-        lblEstetify = new javax.swing.JLabel();
-        lblSidebar = new javax.swing.JLabel();
         lblSubtituloGerenciaMedicos = new javax.swing.JLabel();
-        lblBackgroundCadastro = new javax.swing.JLabel();
         lblTitleGerenciaMedicos = new javax.swing.JLabel();
+        lblBackgroundCadastro = new javax.swing.JLabel();
         lblBackground = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
@@ -139,30 +136,30 @@ public class FrGerenciaProcedimento extends javax.swing.JDialog {
         lblDuracaoEstimada.setForeground(new java.awt.Color(51, 51, 51));
         lblDuracaoEstimada.setText("Duração Estimada");
         getContentPane().add(lblDuracaoEstimada);
-        lblDuracaoEstimada.setBounds(740, 140, 190, 17);
+        lblDuracaoEstimada.setBounds(700, 140, 190, 17);
 
         lblDescricao.setForeground(new java.awt.Color(51, 51, 51));
         lblDescricao.setText("Descrição");
         getContentPane().add(lblDescricao);
-        lblDescricao.setBounds(310, 140, 310, 17);
+        lblDescricao.setBounds(70, 140, 310, 17);
 
         lblRequisitos.setForeground(new java.awt.Color(51, 51, 51));
         lblRequisitos.setText("Requisitos");
         getContentPane().add(lblRequisitos);
-        lblRequisitos.setBounds(310, 210, 200, 17);
+        lblRequisitos.setBounds(60, 210, 200, 17);
 
         lblValor.setForeground(new java.awt.Color(51, 51, 51));
         lblValor.setText("Valor");
         getContentPane().add(lblValor);
-        lblValor.setBounds(990, 140, 230, 17);
+        lblValor.setBounds(980, 140, 230, 17);
 
         fEdtDuracaoEstimada.setText("Duração Estimada");
         getContentPane().add(fEdtDuracaoEstimada);
-        fEdtDuracaoEstimada.setBounds(730, 160, 230, 38);
+        fEdtDuracaoEstimada.setBounds(690, 160, 260, 38);
 
         fEdtValor.setText("Valor");
         getContentPane().add(fEdtValor);
-        fEdtValor.setBounds(980, 160, 290, 38);
+        fEdtValor.setBounds(970, 160, 290, 38);
 
         edtDescricao.setText("Descrição");
         edtDescricao.addActionListener(new java.awt.event.ActionListener() {
@@ -171,7 +168,7 @@ public class FrGerenciaProcedimento extends javax.swing.JDialog {
             }
         });
         getContentPane().add(edtDescricao);
-        edtDescricao.setBounds(300, 160, 410, 40);
+        edtDescricao.setBounds(50, 160, 610, 40);
 
         btnAdicionar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/icons/addsquare.png"))); // NOI18N
         btnAdicionar.setText(" Adicionar");
@@ -181,7 +178,7 @@ public class FrGerenciaProcedimento extends javax.swing.JDialog {
             }
         });
         getContentPane().add(btnAdicionar);
-        btnAdicionar.setBounds(300, 80, 170, 30);
+        btnAdicionar.setBounds(60, 80, 170, 30);
 
         btnSalvar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/icons/save.png"))); // NOI18N
         btnSalvar.setText(" Salvar");
@@ -191,7 +188,7 @@ public class FrGerenciaProcedimento extends javax.swing.JDialog {
             }
         });
         getContentPane().add(btnSalvar);
-        btnSalvar.setBounds(870, 80, 170, 30);
+        btnSalvar.setBounds(690, 80, 170, 30);
 
         btnEditar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/icons/editsquare.png"))); // NOI18N
         btnEditar.setText(" Editar");
@@ -201,7 +198,7 @@ public class FrGerenciaProcedimento extends javax.swing.JDialog {
             }
         });
         getContentPane().add(btnEditar);
-        btnEditar.setBounds(490, 80, 170, 30);
+        btnEditar.setBounds(270, 80, 170, 30);
 
         btnRemover.setIcon(new javax.swing.ImageIcon(getClass().getResource("/icons/trash.png"))); // NOI18N
         btnRemover.setText(" Remover");
@@ -211,20 +208,20 @@ public class FrGerenciaProcedimento extends javax.swing.JDialog {
             }
         });
         getContentPane().add(btnRemover);
-        btnRemover.setBounds(680, 80, 170, 30);
+        btnRemover.setBounds(480, 80, 170, 30);
 
         edtRequisitos.setText("Requisitos");
         getContentPane().add(edtRequisitos);
-        edtRequisitos.setBounds(300, 230, 430, 38);
+        edtRequisitos.setBounds(50, 230, 610, 38);
 
         edtContraIndicacoes.setText("Contraindicações");
         getContentPane().add(edtContraIndicacoes);
-        edtContraIndicacoes.setBounds(750, 230, 520, 38);
+        edtContraIndicacoes.setBounds(690, 230, 570, 38);
 
         lblContraIndicacoes.setForeground(new java.awt.Color(51, 51, 51));
         lblContraIndicacoes.setText("Contraindicações");
         getContentPane().add(lblContraIndicacoes);
-        lblContraIndicacoes.setBounds(760, 210, 400, 17);
+        lblContraIndicacoes.setBounds(700, 210, 400, 17);
 
         grdProcedimentos.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
@@ -245,42 +242,27 @@ public class FrGerenciaProcedimento extends javax.swing.JDialog {
         tmProcedimentos.setViewportView(grdProcedimentos);
 
         getContentPane().add(tmProcedimentos);
-        tmProcedimentos.setBounds(290, 380, 1010, 406);
+        tmProcedimentos.setBounds(40, 370, 1250, 420);
 
-        lblLogo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/icons/logo45x40.png"))); // NOI18N
-        getContentPane().add(lblLogo);
-        lblLogo.setBounds(10, 10, 50, 50);
-
-        lblBackgroundTabela.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/BackgroundCadastro.png"))); // NOI18N
-        lblBackgroundTabela.setText("jLabel18");
+        lblBackgroundTabela.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/backgroundTableModel.png"))); // NOI18N
         getContentPane().add(lblBackgroundTabela);
-        lblBackgroundTabela.setBounds(230, 330, 1120, 500);
-
-        lblEstetify.setFont(new java.awt.Font("Fira Sans Condensed Medium", 0, 18)); // NOI18N
-        lblEstetify.setForeground(new java.awt.Color(51, 51, 51));
-        lblEstetify.setText("Estetify");
-        getContentPane().add(lblEstetify);
-        lblEstetify.setBounds(60, 30, 90, 22);
-
-        lblSidebar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/sidebar.png"))); // NOI18N
-        getContentPane().add(lblSidebar);
-        lblSidebar.setBounds(-460, 0, 750, 900);
+        lblBackgroundTabela.setBounds(-10, 330, 1350, 500);
 
         lblSubtituloGerenciaMedicos.setFont(new java.awt.Font("Fira Sans Medium", 0, 13)); // NOI18N
         lblSubtituloGerenciaMedicos.setForeground(new java.awt.Color(102, 102, 102));
-        lblSubtituloGerenciaMedicos.setText("Gerenciar Médicos");
+        lblSubtituloGerenciaMedicos.setText("Configure e mantenha a lista de procedimentos disponíveis, detalhando preços, duração, requisitos outros.");
         getContentPane().add(lblSubtituloGerenciaMedicos);
-        lblSubtituloGerenciaMedicos.setBounds(280, 40, 210, 17);
-
-        lblBackgroundCadastro.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/BackgroundCad.png"))); // NOI18N
-        getContentPane().add(lblBackgroundCadastro);
-        lblBackgroundCadastro.setBounds(230, 60, 1100, 280);
+        lblSubtituloGerenciaMedicos.setBounds(30, 40, 750, 17);
 
         lblTitleGerenciaMedicos.setFont(new java.awt.Font("Fira Sans SemiBold", 0, 18)); // NOI18N
         lblTitleGerenciaMedicos.setForeground(new java.awt.Color(51, 51, 51));
         lblTitleGerenciaMedicos.setText("Gerenciar Procedimentos");
         getContentPane().add(lblTitleGerenciaMedicos);
-        lblTitleGerenciaMedicos.setBounds(280, 20, 210, 22);
+        lblTitleGerenciaMedicos.setBounds(30, 20, 210, 22);
+
+        lblBackgroundCadastro.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/backgroundCrud.png"))); // NOI18N
+        getContentPane().add(lblBackgroundCadastro);
+        lblBackgroundCadastro.setBounds(-10, 60, 1330, 290);
 
         lblBackground.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/background.png"))); // NOI18N
         lblBackground.setText("jLabel3");
@@ -435,10 +417,7 @@ public class FrGerenciaProcedimento extends javax.swing.JDialog {
     private javax.swing.JLabel lblContraIndicacoes;
     private javax.swing.JLabel lblDescricao;
     private javax.swing.JLabel lblDuracaoEstimada;
-    private javax.swing.JLabel lblEstetify;
-    private javax.swing.JLabel lblLogo;
     private javax.swing.JLabel lblRequisitos;
-    private javax.swing.JLabel lblSidebar;
     private javax.swing.JLabel lblSubtituloGerenciaMedicos;
     private javax.swing.JLabel lblTitleGerenciaMedicos;
     private javax.swing.JLabel lblValor;
