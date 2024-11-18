50193a196d56ed5402041c3c022407d98177f709
VS-7 <vitorsergio.ts@gmail.com>
2024-11-18 18:57:19 -0300

    Atualizando disposicao de componentes View FrGerenciaEnfermeira (Sprint 4)

--- Estetify/src/main/java/com/ifcolab/estetify/view/FrGerenciaEnfermeira.java
+++ Estetify/src/main/java/com/ifcolab/estetify/view/FrGerenciaEnfermeira.java
@@ -135,13 +135,10 @@ public class FrGerenciaEnfermeira extends javax.swing.JDialog {
         btnRemover = new com.ifcolab.estetify.components.SecondaryCustomButton();
         tmEnfermeiras = new javax.swing.JScrollPane();
         grdEnfermeiras = new com.ifcolab.estetify.components.CustomTable();
-        lblLogo = new javax.swing.JLabel();
-        lblBackgroundTabela = new javax.swing.JLabel();
-        lblEstetify = new javax.swing.JLabel();
-        lblSidebar = new javax.swing.JLabel();
         lblSubtituloGerenciaMedicos = new javax.swing.JLabel();
-        lblBackgroundCadastro = new javax.swing.JLabel();
         lblTitleGerenciaMedicos = new javax.swing.JLabel();
+        lblBackgroundTabela = new javax.swing.JLabel();
+        lblBackgroundCadastro = new javax.swing.JLabel();
         lblBackground = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
@@ -156,12 +153,12 @@ public class FrGerenciaEnfermeira extends javax.swing.JDialog {
         lblNome.setForeground(new java.awt.Color(51, 51, 51));
         lblNome.setText("Nome");
         getContentPane().add(lblNome);
-        lblNome.setBounds(310, 140, 130, 17);
+        lblNome.setBounds(70, 140, 130, 17);
 
         lblEmail.setForeground(new java.awt.Color(51, 51, 51));
         lblEmail.setText("Email");
         getContentPane().add(lblEmail);
-        lblEmail.setBounds(600, 140, 130, 17);
+        lblEmail.setBounds(480, 140, 130, 17);
 
         lblSexo.setForeground(new java.awt.Color(51, 51, 51));
         lblSexo.setText("Sexo");
@@ -171,17 +168,17 @@ public class FrGerenciaEnfermeira extends javax.swing.JDialog {
         lblDataNascimento.setForeground(new java.awt.Color(51, 51, 51));
         lblDataNascimento.setText("Data de Nascimento");
         getContentPane().add(lblDataNascimento);
-        lblDataNascimento.setBounds(310, 210, 170, 17);
+        lblDataNascimento.setBounds(70, 210, 170, 17);
 
         lblTelefone.setForeground(new java.awt.Color(51, 51, 51));
         lblTelefone.setText("Telefone");
         getContentPane().add(lblTelefone);
-        lblTelefone.setBounds(800, 210, 140, 17);
+        lblTelefone.setBounds(720, 210, 140, 17);
 
         lblEndereco.setForeground(new java.awt.Color(51, 51, 51));
         lblEndereco.setText("Endereço");
         getContentPane().add(lblEndereco);
-        lblEndereco.setBounds(510, 210, 140, 17);
+        lblEndereco.setBounds(300, 210, 240, 17);
 
         lblCOREN.setForeground(new java.awt.Color(51, 51, 51));
         lblCOREN.setText("COREN");
@@ -204,7 +201,7 @@ public class FrGerenciaEnfermeira extends javax.swing.JDialog {
             }
         });
         getContentPane().add(edtEmail);
-        edtEmail.setBounds(590, 160, 270, 40);
+        edtEmail.setBounds(470, 160, 380, 40);
 
         edtNome.setText("Nome");
         edtNome.addActionListener(new java.awt.event.ActionListener() {
@@ -213,11 +210,11 @@ public class FrGerenciaEnfermeira extends javax.swing.JDialog {
             }
         });
         getContentPane().add(edtNome);
-        edtNome.setBounds(300, 160, 270, 40);
+        edtNome.setBounds(60, 160, 380, 40);
 
         fEdtDataNascimento.setText("Data de Nascimento");
         getContentPane().add(fEdtDataNascimento);
-        fEdtDataNascimento.setBounds(300, 230, 180, 38);
+        fEdtDataNascimento.setBounds(60, 230, 200, 38);
 
         fEdtCPF.setText("CPF");
         getContentPane().add(fEdtCPF);
@@ -225,7 +222,7 @@ public class FrGerenciaEnfermeira extends javax.swing.JDialog {
 
         fEdtTelefone.setText("Telefone");
         getContentPane().add(fEdtTelefone);
-        fEdtTelefone.setBounds(790, 230, 208, 38);
+        fEdtTelefone.setBounds(710, 230, 280, 38);
 
         edtCOREN.setText("COREN");
         edtCOREN.addActionListener(new java.awt.event.ActionListener() {
@@ -243,7 +240,7 @@ public class FrGerenciaEnfermeira extends javax.swing.JDialog {
             }
         });
         getContentPane().add(edtEndereco);
-        edtEndereco.setBounds(500, 230, 270, 40);
+        edtEndereco.setBounds(290, 230, 390, 40);
 
         btnAdicionar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/icons/addsquare.png"))); // NOI18N
         btnAdicionar.setText(" Adicionar");
@@ -253,7 +250,7 @@ public class FrGerenciaEnfermeira extends javax.swing.JDialog {
             }
         });
         getContentPane().add(btnAdicionar);
-        btnAdicionar.setBounds(300, 80, 170, 30);
+        btnAdicionar.setBounds(60, 80, 170, 30);
 
         btnSalvar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/icons/save.png"))); // NOI18N
         btnSalvar.setText(" Salvar");
@@ -263,7 +260,7 @@ public class FrGerenciaEnfermeira extends javax.swing.JDialog {
             }
         });
         getContentPane().add(btnSalvar);
-        btnSalvar.setBounds(870, 80, 170, 30);
+        btnSalvar.setBounds(690, 80, 170, 30);
 
         btnEditar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/icons/editsquare.png"))); // NOI18N
         btnEditar.setText(" Editar");
@@ -273,7 +270,7 @@ public class FrGerenciaEnfermeira extends javax.swing.JDialog {
             }
         });
         getContentPane().add(btnEditar);
-        btnEditar.setBounds(490, 80, 170, 30);
+        btnEditar.setBounds(270, 80, 170, 30);
 
         btnRemover.setIcon(new javax.swing.ImageIcon(getClass().getResource("/icons/trash.png"))); // NOI18N
         btnRemover.setText(" Remover");
@@ -283,7 +280,7 @@ public class FrGerenciaEnfermeira extends javax.swing.JDialog {
             }
         });
         getContentPane().add(btnRemover);
-        btnRemover.setBounds(680, 80, 170, 30);
+        btnRemover.setBounds(480, 80, 170, 30);
 
         grdEnfermeiras.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
@@ -304,42 +301,27 @@ public class FrGerenciaEnfermeira extends javax.swing.JDialog {
         tmEnfermeiras.setViewportView(grdEnfermeiras);
 
         getContentPane().add(tmEnfermeiras);
-        tmEnfermeiras.setBounds(290, 380, 1010, 406);
-
-        lblLogo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/icons/logo45x40.png"))); // NOI18N
-        getContentPane().add(lblLogo);
-        lblLogo.setBounds(10, 10, 50, 50);
-
-        lblBackgroundTabela.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/BackgroundCadastro.png"))); // NOI18N
-        lblBackgroundTabela.setText("jLabel18");
-        getContentPane().add(lblBackgroundTabela);
-        lblBackgroundTabela.setBounds(230, 330, 1120, 500);
-
-        lblEstetify.setFont(new java.awt.Font("Fira Sans Condensed Medium", 0, 18)); // NOI18N
-        lblEstetify.setForeground(new java.awt.Color(51, 51, 51));
-        lblEstetify.setText("Estetify");
-        getContentPane().add(lblEstetify);
-        lblEstetify.setBounds(60, 30, 90, 22);
-
-        lblSidebar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/sidebar.png"))); // NOI18N
-        getContentPane().add(lblSidebar);
-        lblSidebar.setBounds(-460, 0, 750, 900);
+        tmEnfermeiras.setBounds(40, 370, 1260, 420);
 
         lblSubtituloGerenciaMedicos.setFont(new java.awt.Font("Fira Sans Medium", 0, 13)); // NOI18N
         lblSubtituloGerenciaMedicos.setForeground(new java.awt.Color(102, 102, 102));
-        lblSubtituloGerenciaMedicos.setText("Gerenciar Médicos");
+        lblSubtituloGerenciaMedicos.setText("Controle as informações das enfermeiras e dados pessoais.");
         getContentPane().add(lblSubtituloGerenciaMedicos);
-        lblSubtituloGerenciaMedicos.setBounds(280, 40, 210, 17);
-
-        lblBackgroundCadastro.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/BackgroundCad.png"))); // NOI18N
-        getContentPane().add(lblBackgroundCadastro);
-        lblBackgroundCadastro.setBounds(230, 60, 1100, 280);
+        lblSubtituloGerenciaMedicos.setBounds(30, 40, 720, 17);
 
         lblTitleGerenciaMedicos.setFont(new java.awt.Font("Fira Sans SemiBold", 0, 18)); // NOI18N
         lblTitleGerenciaMedicos.setForeground(new java.awt.Color(51, 51, 51));
         lblTitleGerenciaMedicos.setText("Gerenciar Enfermeiras");
         getContentPane().add(lblTitleGerenciaMedicos);
-        lblTitleGerenciaMedicos.setBounds(280, 20, 210, 22);
+        lblTitleGerenciaMedicos.setBounds(30, 20, 210, 22);
+
+        lblBackgroundTabela.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/backgroundTableModel.png"))); // NOI18N
+        getContentPane().add(lblBackgroundTabela);
+        lblBackgroundTabela.setBounds(-10, 330, 1350, 500);
+
+        lblBackgroundCadastro.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/backgroundCrud.png"))); // NOI18N
+        getContentPane().add(lblBackgroundCadastro);
+        lblBackgroundCadastro.setBounds(-10, 60, 1330, 290);
 
         lblBackground.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/background.png"))); // NOI18N
         lblBackground.setText("jLabel3");
@@ -525,11 +507,8 @@ public class FrGerenciaEnfermeira extends javax.swing.JDialog {
     private javax.swing.JLabel lblDataNascimento;
     private javax.swing.JLabel lblEmail;
     private javax.swing.JLabel lblEndereco;
-    private javax.swing.JLabel lblEstetify;
-    private javax.swing.JLabel lblLogo;
     private javax.swing.JLabel lblNome;
     private javax.swing.JLabel lblSexo;
-    private javax.swing.JLabel lblSidebar;
     private javax.swing.JLabel lblSubtituloGerenciaMedicos;
     private javax.swing.JLabel lblTelefone;
     private javax.swing.JLabel lblTitleGerenciaMedicos;
