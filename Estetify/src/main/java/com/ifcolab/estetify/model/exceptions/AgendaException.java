23e1540870f5a8274ff4a7483aa8b80d4a93a302
VS-7 <vitorsergio.ts@gmail.com>
2024-11-19 19:22:58 -0300

    Implementando AgendaException (Sprint 4)

--- Estetify/src/main/java/com/ifcolab/estetify/model/exceptions/AgendaException.java
+++ Estetify/src/main/java/com/ifcolab/estetify/model/exceptions/AgendaException.java
@@ -1,1 +1,7 @@
- 
+ package com.ifcolab.estetify.model.exceptions;
+
+public class AgendaException extends RuntimeException {
+    public AgendaException(String msg) {
+        super(msg);
+    }
+} 
