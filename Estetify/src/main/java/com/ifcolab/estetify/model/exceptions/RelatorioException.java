3d4fdfd456d4909c6d2f480f37049507232e120d
VS-7 <vitorsergio.ts@gmail.com>
2024-12-30 20:44:29 -0300

    Implementando RelatorioException (Sprint 04)

---
+++ Estetify/src/main/java/com/ifcolab/estetify/model/exceptions/RelatorioException.java
@@ -0,0 +1,8 @@
+package com.ifcolab.estetify.model.exceptions;
+
+public class RelatorioException extends RuntimeException {
+    
+    public RelatorioException(String message) {
+        super(message);
+    }
+}
