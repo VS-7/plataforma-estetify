407d5045b62d9c33db0ecb49dd52f9861f654534
VS-7 <vitorsergio.ts@gmail.com>
2024-12-31 12:09:12 -0300

    Implementacao PagamentoException (Sprint 04)

--- Estetify/src/main/java/com/ifcolab/estetify/model/exceptions/PagamentoException.java
+++ Estetify/src/main/java/com/ifcolab/estetify/model/exceptions/PagamentoException.java
@@ -1,1 +1,7 @@
- 
+package com.ifcolab.estetify.model.exceptions;
+
+public class PagamentoException extends RuntimeException {
+    public PagamentoException(String msg) {
+        super(msg);
+    }
+} 
