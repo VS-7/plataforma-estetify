761ec6f67397b1aca21bbca9de13c4910117330a
VS-7 <vitorsergio.ts@gmail.com>
2024-12-31 12:09:57 -0300

    Implementacao ValidatePagamento (Sprint 04)

--- Estetify/src/main/java/com/ifcolab/estetify/model/valid/ValidatePagamento.java
+++ Estetify/src/main/java/com/ifcolab/estetify/model/valid/ValidatePagamento.java
@@ -1,13 +1,50 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.ifcolab.estetify.model.valid;
 
-/**
- *
- * @author vitorsrgio
- */
+import com.ifcolab.estetify.model.Pagamento;
+import com.ifcolab.estetify.model.Consulta;
+import com.ifcolab.estetify.model.Procedimento;
+import com.ifcolab.estetify.model.Pessoa;
+import com.ifcolab.estetify.model.exceptions.PagamentoException;
+import com.ifcolab.estetify.model.enums.StatusPagamento;
+import com.ifcolab.estetify.model.enums.MetodoPagamento;
+import java.time.LocalDateTime;
+
 public class ValidatePagamento {
     
+    public Pagamento validaCamposEntrada(
+            double valor,
+            StatusPagamento status,
+            MetodoPagamento metodoPagamento,
+            String detalhes,
+            Consulta consulta,
+            Procedimento procedimento,
+            Pessoa registrador
+    ) {
+        if (valor <= 0) {
+            throw new PagamentoException("Valor deve ser maior que zero.");
+        }
+        
+        if (status == null) {
+            throw new PagamentoException("Status do pagamento não pode estar em branco.");
+        }
+        
+        if (metodoPagamento == null) {
+            throw new PagamentoException("Método de pagamento não pode estar em branco.");
+        }
+        
+        if (detalhes != null && detalhes.length() > 500) {
+            throw new PagamentoException("Detalhes não podem ter mais que 500 caracteres.");
+        }
+        
+        if (consulta == null && procedimento == null) {
+            throw new PagamentoException("É necessário associar o pagamento a uma consulta ou procedimento.");
+        }
+        
+        if (registrador == null) {
+            throw new PagamentoException("Registrador do pagamento não pode estar em branco.");
+        }
+        
+        return new Pagamento(0, valor, status, metodoPagamento, LocalDateTime.now(), 
+                           detalhes, consulta, procedimento, registrador);
+    }
 }
